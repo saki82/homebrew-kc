5a08c835c7d76e6a41474d85d683224634f350f5
Athanassios Anastasiadis <anastasiadis@me.com>
2026-03-11 00:10:10 +0100

    fix: strip sha256 prefix from formula

--- Formula/kc.rb
+++ Formula/kc.rb
@@ -2,7 +2,7 @@ class Kc < Formula
   desc "Encrypted secret store synced via iCloud Drive"
   homepage "https://github.com/saki82/kc"
   url "https://github.com/saki82/kc/releases/download/v1.0.0/kc-v1.0.0-macos.tar.gz"
-  sha256 "sha256:288ba2c9a9ad0aae58e47c1073e67a2edb6d9f079e3f3b779fb349c94af1db4b"
+  sha256 "288ba2c9a9ad0aae58e47c1073e67a2edb6d9f079e3f3b779fb349c94af1db4b"
   license "MIT"
   version "1.0.0"
 
