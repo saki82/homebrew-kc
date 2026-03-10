182c0f7e5f1ab14a3f31e26e568e12c928b80190
Athanassios Anastasiadis <anastasiadis@me.com>
2026-03-11 00:01:07 +0100

    added v1 tarball SHA

--- Formula/kc.rb
+++ Formula/kc.rb
@@ -2,7 +2,7 @@ class Kc < Formula
   desc "Encrypted secret store synced via iCloud Drive"
   homepage "https://github.com/saki82/kc"
   url "https://github.com/saki82/kc/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "REPLACE_WITH_SHA256_FROM_RELEASE_NOTES"
+  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   license "MIT"
 
   depends_on xcode: ["14.0", :build]
