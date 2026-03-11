a5b13a1ec27205811ff7a1a175935ff23dde0141
Athanassios Anastasiadis <anastasiadis@me.com>
2026-03-11 01:02:19 +0100

    release: v1.1.1

--- Formula/kc.rb
+++ Formula/kc.rb
@@ -4,7 +4,7 @@ class Kc < Formula
   url "https://github.com/saki82/kc/releases/download/v1.1.1/kc-v1.1.1-macos.tar.gz"
   sha256 "53d836a37650a1cf52e51e2960c17929450d8f67fae0294638accaf2682c71fa"
   license "MIT"
-  version "1.1.0"
+  version "1.1.1"
 
   depends_on :macos
 
